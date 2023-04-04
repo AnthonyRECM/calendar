d3148af35f068db92cab553c259fe6857b6ffe91
SirObby <70753342+SirObby@users.noreply.github.com>
2023-04-04 09:47:04 +0300

    Fix GitHub auth

--- application.php
+++ application.php
@@ -2,10 +2,12 @@
 
 require_once "secrets.php";
 
+# URL of github api
 $authorizeURL = 'https://github.com/login/oauth/authorize';
 $tokenURL = 'https://github.com/login/oauth/access_token';
 $apiURLBase = 'https://api.github.com/';
 
+# start sessions
 session_start();
 
 // Start the login process by sending the user to Github's authorization page
@@ -24,6 +26,14 @@ if(get('action') == 'login') {
   // Redirect the user to Github's authorization page
   header('Location: ' . $authorizeURL . '?' . http_build_query($params));
   die();
+}
+
+// to kill all Sessions and reset code base 
+if(get('action') == 'exit') {
+    unset($_SESSION['state']);
+    unset($_SESSION['access_token']);
+    session_destroy();
+    exit();
 }
 
 // When Github redirects the user back here, there will be a "code" and "state" parameter in the query string
@@ -47,43 +57,63 @@ if(get('code')) {
   header('Location: ' . $_SERVER['PHP_SELF']);
 }
 
+# if successful show results
 if(session('access_token')) {
-  $user = apiRequest($apiURLBase . 'user');
+  $user = apiRequest($apiURLBase.'user');
 
   echo '<h3>Logged In</h3>';
-  echo '<h4>' . $user->name . '</h4>';
+  echo '<h4>' . $user->login . '</h4>';
   echo '<pre>';
   print_r($user);
   echo '</pre>';
 
+  
+#print out full list of urls of github  
+  print '<br /><br />';
+  print '<h3>Full List of Urls on Github</h3>';
+  $full = apiRequest($apiURLBase);
+  foreach ($full as $key=>$value)
+  {
+      print $key .'=>'. $value.'<br />';
+  }
+
 } else {
+
+# fail result if no session token
   echo '<h3>Not logged in</h3>';
   echo '<p><a href="?action=login">Log In</a></p>';
 }
 
-
+# main function for curl requests
 function apiRequest($url, $post=FALSE, $headers=array()) {
   $ch = curl_init($url);
+
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Linux useragent'); //change agent string
 
   if($post)
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
 
   $headers[] = 'Accept: application/json';
 
+  # add access token to header 
   if(session('access_token'))
     $headers[] = 'Authorization: Bearer ' . session('access_token');
 
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
   $response = curl_exec($ch);
-  return json_decode($response);
+  return json_decode($response); //decode response
 }
 
+# array key existence
 function get($key, $default=NULL) {
   return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 }
 
+# array key existence
 function session($key, $default=NULL) {
   return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 }
+?>
